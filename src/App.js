e262c3256279c7e55b48104f2983f32bd71512b9
sam-phillips21 <phillips.sam@gmail.com>
2023-04-26 11:22:48 -0600

    refactor render

--- src/App.js
+++ src/App.js
@@ -40,14 +40,17 @@ class App extends Component {
   render() {
     console.log('Render')
 
-    const filteredMonsters = this.state.monsters.filter((monster)=> { return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } =this;
+    const filteredMonsters = monsters.filter((monster)=> { 
+      return monster.name.toLocaleLowerCase().includes(searchField);
             
     });
 
     return (
       <div className="App">
         <input className='search-box' type= 'search' placeholder='search monsters' 
-        onChange={this.onSearchChange} 
+        onChange={onSearchChange} 
         />
          {filteredMonsters.map((monster) => {
             return(
