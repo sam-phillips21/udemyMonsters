7c142501c8a16143dd669a6e00c6c0aa095e83b5
sam-phillips21 <phillips.sam@gmail.com>
2023-04-26 16:54:01 -0600

    component refactors

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,12 @@ import { Component } from "react";
 class CardList extends Component {
 
     render() {
-        return <div>Hello I'm the CardList Component</div>;
+        return (
+            <div>
+                <h1>Hello I'm the CardList Component</h1>
+                
+            </div>
+        )
     }
 }
 
