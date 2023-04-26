f24cf83f8b3d4406cc9759764b22ba4e7b143300
sam-phillips21 <phillips.sam@gmail.com>
2023-04-26 11:14:40 -0600

    update to search feature

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
       this.state = {
         monsters: [],
+        searchField: '',
       };
       console.log('constructor')
   }
@@ -31,22 +32,22 @@ class App extends Component {
 
   render() {
     console.log('Render')
+
+    const filteredMonsters = this.state.monsters.filter((monster)=> { return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+            
+    });
+
     return (
       <div className="App">
         <input className='search-box' type= 'search' placeholder='search monsters' onChange={(event) => {
-          console.log({startingArray: this.state.monsters});
-          const searchString = event.target.value.toLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster)=> { return monster.name.toLocaleLowerCase().includes(searchString);
-            
-          });
-
+          const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return {monsters: filteredMonsters};
+              return {searchField};
             })
 
         }} 
         />
-         {this.state.monsters.map((monster) => {
+         {filteredMonsters.map((monster) => {
             return(
               <div key= {monster.id}>
                 <h1>{monster.name}</h1>
